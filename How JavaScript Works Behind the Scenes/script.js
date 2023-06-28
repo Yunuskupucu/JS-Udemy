aeb5dddccb50d99eb65883227cf47ff04dbb9f16
Yunuskupucu <yunus.kupucu@gmail.com>
2023-06-28 17:30:36 +0300

    Copying objects

--- How JavaScript Works Behind the Scenes/script.js
+++ How JavaScript Works Behind the Scenes/script.js
@@ -90,3 +90,20 @@ friend.age = 27;
 console.log('me', me);
 console.log('friend', friend) ;
 */
+
+// Copying objects
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Marry');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica);
+console.log('After marriage:', jessicaCopy);
