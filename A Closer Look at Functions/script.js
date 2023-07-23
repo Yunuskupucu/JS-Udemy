2ac5e5b3b3e4c9e92abb5ba001b52c5fea13dace
Yunuskupucu <yunus.kupucu@gmail.com>
2023-07-23 12:12:36 +0300

    functions

---
+++ A Closer Look at Functions/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
