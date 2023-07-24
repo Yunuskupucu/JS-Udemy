9724651f54d55e25d12f1dc6b373af8a3dde2ae5
Yunuskupucu <yunus.kupucu@gmail.com>
2023-07-24 15:18:37 +0300

    Arguments Works

--- A Closer Look at Functions/script.js
+++ A Closer Look at Functions/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const bookings = [];
+// Default Parameters
+/* const bookings = [];
 
 const createBooking = function (
   flightNum,
@@ -21,3 +22,37 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+ */
+
+// How Passing Arguments Works: Value vs. Reference
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 4423124324,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 4423124324) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
