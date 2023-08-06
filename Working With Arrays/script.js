6fac65849aed20e2836dca8a002befc8221b1139
Yunuskupucu <yunus.kupucu@gmail.com>
2023-08-06 19:02:11 +0300

    reduce methods

--- Working With Arrays/script.js
+++ Working With Arrays/script.js
@@ -79,3 +79,11 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+// max value
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
